70dc4689a3a922f89ea5607a77e7d35b215a040e
Ryan Camp <campryan@comcast.net>
2020-03-19 13:01:35 -0600

    Refactor shelters_controller with strong params

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -11,12 +11,7 @@ class SheltersController < ApplicationController
   end
 
   def create
-    shelter = Shelter.create({name:params[:name],
-                             address:params[:address],
-                             city:params[:city],
-                             state:params[:state],
-                             zip:params[:zip]})
-    shelter.save
+    Shelter.create(shelter_params)
     redirect_to '/shelters'
   end
 
@@ -25,17 +20,17 @@ class SheltersController < ApplicationController
 
   def update
     shelter = Shelter.find(params[:id])
-    shelter.update({name: params[:name],
-                   address: params[:address],
-                   city: params[:city],
-                   state: params[:state],
-                   zip: params[:zip]})
-    shelter.save
+    shelter.update(shelter_params)
     redirect_to "/shelters/#{shelter.id}"
   end
 
-  def delete
+  def destroy
     Shelter.destroy(params[:id])
     redirect_to '/shelters'
+  end
+
+  private
+  def shelter_params
+    params.permit(:name, :address, :city, :state, :zip)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   get '/shelters/:id/edit', to: 'shelters#edit'
   patch '/shelters/:id', to: 'shelters#update'
 
-  delete '/shelters/:id', to: 'shelters#delete'
+  delete '/shelters/:id', to: 'shelters#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
